ca92fba637aed900d76cf2da831f20dedcd559d7
zigth <zigrand.thierry.001@student.uni.lu>
2019-05-15 18:06:45 +0200

    updated test

--- depthNN.py
+++ depthNN.py
@@ -27,13 +27,16 @@ print(tf.test.is_built_with_cuda())
 #output_training_generator = output_training_generator.reshape(87,216)
 #output_validation_generator = output_validation_generator.reshape(21,216)
 
+#data_generator = ImageDataGenerator(validation_split=0.2,
+#                               rotation_range=5,
+#                               width_shift_range=0.1,
+#                               height_shift_range=0.1,
+#                               shear_range=0.1,
+#                               zoom_range=0.1,
+#                               horizontal_flip=False,
+#                               rescale=1. / 255)
+
 data_generator = ImageDataGenerator(validation_split=0.2,
-                               rotation_range=15,
-                               width_shift_range=0.2,
-                               height_shift_range=0.2,
-                               shear_range=0.2,
-                               zoom_range=0.2,
-                               horizontal_flip=True,
                                rescale=1. / 255)
 
 train_set_size=83457
@@ -98,8 +101,8 @@ model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D())
 model.add(Dropout(0.1))
 
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
 
 
 model.add(ZeroPadding2D(((1, 1), (1, 1))))
@@ -118,15 +121,15 @@ model.compile(loss='mean_squared_error',
               metrics=['accuracy'])
 
 print(model.summary())
-mycallback = keras.callbacks.ModelCheckpoint(filepath = 'models/mymodel3_{epoch:02d}-{val_loss:.2f}.h5', monitor='val_loss', verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=1)
-csv_logger = keras.callbacks.CSVLogger('mymodel3.log',append=True)
+mycallback = keras.callbacks.ModelCheckpoint(filepath = 'models/mymodel7_{epoch:02d}-{val_loss:.2f}.h5', monitor='val_loss', verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=1)
+csv_logger = keras.callbacks.CSVLogger('mymodel7.log',append=True)
 
 #score = model.evaluate(np.array(input_validation_generator), np.array(output_validation_generator), batch_size=32)
-if os.path.exists('models/mymodel3_03-0.05.h5'):
-    model = keras.models.load_model("models/mymodel3_03-0.05.h5")
+#if os.path.exists('models/mymodel4_03-0.05.h5'):
+#    model = keras.models.load_model("models/mymodel4_03-0.05.h5")
 
-if os.path.exists('mymodel3.h5'):
-    model=keras.models.load_model("mymodel3.h5")
+if os.path.exists('models/mymodel7.h5'):
+    model=keras.models.load_model("models/mymodel7.h5")
 else:
     model.fit_generator(
 		training_generator,
@@ -135,25 +138,6 @@ else:
         validation_data=validation_generator,
         validation_steps=val_set_size // batch_size,
         callbacks=[mycallback,csv_logger])
-    model.save('models/mymodel3.h5')
+    model.save('models/mymodel7.h5')
 
 
-img_path = "data/input/scene/ims_10002196_93457.png"
-img = image.load_img(img_path, target_size=(144, 256))
-#print(type(img))
-
-x = image.img_to_array(img)/255.
-#print(type(x))
-#print(x.shape)
-plt.imshow(x)
-plt.show()
-
-x = np.expand_dims(x, axis=0)
-#print(x.shape)
-
-
-img_depth = model.predict(x).reshape(144,256)
-print(img_depth)
-
-plt.imshow((img_depth))
-plt.show()
--- depthTest.py
+++ depthTest.py
@@ -7,6 +7,7 @@ from keras.optimizers import SGD,Adam
 import matplotlib.pyplot as plt
 #from PIL import Image
 import os
+import random
 #import imageio
 
 # Generate dummy data
@@ -15,42 +16,101 @@ import numpy as np
 import tensorflow as tf
 print(tf.test.is_built_with_cuda())
 
+data_generator = ImageDataGenerator(validation_split=0.2,
+                               rescale=1. / 255)
 
-if os.path.exists('models/mymodel.h5'):
-    model=keras.models.load_model("models/mymodel.h5")
+train_set_size=83457
+val_set_size=20864
+batch_size=16
+
+def generator_training_inout(generator, dirIn, dirOut, batch_size, img_height, img_width):
+    genI = generator.flow_from_directory(dirIn, subset='training',
+                                          target_size=(img_height, img_width),
+                                          class_mode='categorical',
+                                          batch_size=batch_size,
+                                          shuffle=False)
+
+    genO = generator.flow_from_directory(dirOut, subset='training',
+                                          target_size=(img_height, img_width),
+                                          color_mode="grayscale",
+                                          class_mode='categorical',
+                                          batch_size=batch_size,
+                                          shuffle=False)
+    while True:
+        X = genI.next()
+        Y = genO.next()
+        yield X[0], Y[0]
+
+def generator_validation_inout(generator, dirIn, dirOut, batch_size, img_height, img_width):
+    genI = generator.flow_from_directory(dirIn, subset='validation',
+                                          target_size=(img_height, img_width),
+                                          class_mode='categorical',
+                                          batch_size=batch_size,
+                                          shuffle=False)
+
+    genO = generator.flow_from_directory(dirOut, subset='validation',
+                                          target_size=(img_height, img_width),
+                                          color_mode="grayscale",
+                                          class_mode='categorical',
+                                          batch_size=batch_size,
+                                          shuffle=False)
+    while True:
+        X = genI.next()
+        Y = genO.next()
+        yield X[0], Y[0]
 
 
+#training_generator = generator_training_inout(data_generator,'data/input','data/output',batch_size,144,256)
+validation_generator = generator_validation_inout(data_generator,'data/input','data/output',batch_size,144,256)
+
+
+if os.path.exists('models/mymodel5.h5'):
+    model=keras.models.load_model("models/mymodel5.h5")
+
+#for i in range(random.randint(0,1000)):
+#	data=next(validation_generator)
 
 fig = plt.figure(figsize=(12,15))
 		
-img_path = "data/input/scene/ims_10002382_42314.png"
-img = image.load_img(img_path, target_size=(144, 256))
+#img_path = "data/input/scene/ims_10002382_42314.png"
+#img = image.load_img(img_path, target_size=(144, 256))
 #print(type(img))
 
-x = image.img_to_array(img)/255.
+#x = image.img_to_array(img)/255.
 #print(type(x))
 #print(x.shape)
-fig.add_subplot(3,1,1)
-plt.imshow(x)
+#fig.add_subplot(3,1,1)
+#plt.imshow(x)
 #plt.show()
 
-x = np.expand_dims(x, axis=0)
+#x = np.expand_dims(x, axis=0)
 #print(x.shape)
 
-fig.add_subplot(3,1,2)
-img_depth = model.predict(x).reshape(144,256)
-print(img_depth)
+#fig.add_subplot(3,1,2)
+#img_depth = model.predict(x).reshape(144,256)
+#print(img_depth)
 
-plt.imshow(img_depth,cmap='gray')
+#plt.imshow(img_depth,cmap='gray')
 #plt.show()
 
-img_path = "data/output/depth/imd_10002382_42314.png"
-img = image.load_img(img_path, target_size=(144, 256))
+#img_path = "data/output/depth/imd_10002382_42314.png"
+#img = image.load_img(img_path, target_size=(144, 256))
 #print(type(img))
 
-x = np.array(image.img_to_array(img)/255.)
+#x = np.array(image.img_to_array(img)/255.)
 #print(type(x))
 #print(x.shape)
-fig.add_subplot(3,1,3)
-plt.imshow(x,cmap='jet')
+#fig.add_subplot(3,1,3)
+#plt.imshow(x,cmap='jet')
+#plt.show()
+
+for i in range(0,4):
+    data=next(validation_generator)
+    fig.add_subplot(4, 3, i*3+1)
+    plt.imshow(data[0][0])
+    fig.add_subplot(4, 3, i*3+2)
+    plt.imshow(model.predict(data[0])[0].reshape(144,256),cmap='jet')
+    fig.add_subplot(4, 3, i * 3 + 3)
+    plt.imshow(data[1][0].reshape(144,256),cmap='jet')
+
 plt.show()
